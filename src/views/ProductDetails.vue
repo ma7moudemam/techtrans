a56a81e8efd4ff9e01bca34b04be79f9cda83700
mahmoud-emam97 <m7moud_emam@hotmail.com>
2024-11-17 13:41:38 +0200

    add product details page

---
+++ src/views/ProductDetails.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="ProductDetails">
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "ProductDetails",
+  props: {
+    msg: String,
+  },
+  components: {
+  
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
